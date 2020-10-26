52bd48692b37d579bac8f86803c6ab5b71229f24
Julius <kohwoojin@gmail.com>
2020-10-26 16:15:31 -0400

    More container spacing

--- c1_frontend/src/components/Content/Content.js
+++ c1_frontend/src/components/Content/Content.js
@@ -11,7 +11,7 @@ const useStyles = makeStyles(({ spacing }) => ({
   },
   autoSpacing: {
     "& > *": {
-      marginBottom: spacing(1),
+      marginBottom: spacing(2),
     },
   },
   padding: {
