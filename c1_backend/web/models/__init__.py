ccc21074061157d24b4277fbb634b32cb40152e3
ds <deerff@gmail.com>
2021-01-21 03:57:30 -0800

    add jobs, owners, facilities, bids tables

--- c1_backend/web/models/__init__.py
+++ c1_backend/web/models/__init__.py
@@ -7,3 +7,7 @@ from .inspectors import Inspector
 from .inspector_logins import InspectorLogin
 from .certifications import Certification
 from .parent_organizations import ParentOrganization
+from .jobs import Job
+from .owners import Owner
+from .facilities import Facility
+from .bids import Bid
